c64bedc0258916da574613db695bb5202d236d6e
Zhiyuan Song <songalbatross@gmail.com>
2016-08-15 10:55:40 -0700

    add R files

---
+++ TrainStatus.R
@@ -0,0 +1,6 @@
+consumer_key <- "WrvZpySST9ozDztvldEBi8Ibl"
+consumer_secret <- "0pGP603uWcbthQTdM8arOF34YjhPCYwblx3oj2rYbjAuuBLSpK"
+access_token <- "47530544-VMIwUqvqULzcdBe6vEO3lePiL4YYEq2LswpJUYP2C"
+access_secret <- "w5708WDaVK6mATGshYJNwFupAJ7AxZkCJ9XbzHGRvvV6f"
+
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
