ac956f31f7889888d7a11b5eb9e3570770362f71
Zhiyuan Song <songalbatross@gmail.com>
2016-08-22 12:03:35 -0700

    update with extracted train information

--- TrainStatus.R
+++ TrainStatus.R
@@ -2,6 +2,7 @@ library(twitteR)
 library(dplyr)
 library(chron)
 library(stringr)
+require(bit64)
 
 consumer_key <- "WrvZpySST9ozDztvldEBi8Ibl"
 consumer_secret <- "0pGP603uWcbthQTdM8arOF34YjhPCYwblx3oj2rYbjAuuBLSpK"
@@ -9,18 +10,68 @@ access_token <- "47530544-VMIwUqvqULzcdBe6vEO3lePiL4YYEq2LswpJUYP2C"
 access_secret <- "w5708WDaVK6mATGshYJNwFupAJ7AxZkCJ9XbzHGRvvV6f"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-timeline<-userTimeline("CaltrainStatus", n=3000)
-timeline.df<-twListToDF(timeline)
+
+#Retrieve the timeline of tweets by @CaltrainStatus
+timeline<-function(userID="CaltrainStatus", nQuery=3200, sinceDate="2014-08-17"){
+  tl<-userTimeline(userID, n=nQuery)
+  tl.df<-twListToDF(tl)
+  while (as.Date(tail(tl.df$created, 1))>sinceDate) {
+    tl<-userTimeline(userID, n=nQuery, maxID = as.integer64(tail(tl.df$id, 1))-1)
+    if (length(tl)==0) break
+    tl.df<-rbind(tl.df, twListToDF(tl))
+  }
+  return(tl.df)
+}
+timeline.df<-timeline()
+DateHour<-round(timeline.df$created, units="hours")
+
 tw.df<-select(timeline.df, c(text, created))
-tw.df$created<-as.POSIXlt(tw.df$created, tz="America/Los_Angeles")
-tw.df$hour<-(tw.df$created)$hour
+
+#Extract train ID
+tw.df$trainID<-NA
+text.split<-str_split_fixed(tw.df$text, "@", 2)
+tw.df$text<-text.split[,1]
+tw.df$text<-gsub("[0-9]{4,}", "Numbers", x=tw.df$text)
+id.rows<-grep("[12348][0-9]{2}", tw.df$text)
+id.position<-regexpr("[12348][0-9]{2}", tw.df$text)
+id.train<-regmatches(tw.df$text, id.position)
+tw.df<-tw.df[id.rows,] #keep only the rows with train IDs
+tw.df$trainID<-id.train
+
+
+#derive train direction (nb or sb)
+tw.df$direction<-NA
+tw.df$direction<-ifelse(as.integer(tw.df$trainID)%%2==0, "sb", "nb")
+
+#derive train types (local, limited or bullet)
+tw.df$trainType<-NA
+local.rows<-grep("^[14]", tw.df$trainID)
+limited.rows<-grep("^2", tw.df$trainID)
+bullet.rows<-grep("^[38]", tw.df$trainID)
+tw.df$trainType[local.rows]<-"local"
+tw.df$trainType[limited.rows]<-"limited"
+tw.df$trainType[bullet.rows]<-"bullet"
+
+#Extract time information
+tw.df$created<-round(as.POSIXlt(tw.df$created, tz="America/Los_Angeles"), units="hours")
+tw.df$hour<-tw.df$created$hour
 tw.df$date<-as.Date(tw.df$created)
-tw.df$date<-chron(dates.=as.character(as.Date(tw.df$created)), format = "y-m-d")
+#tw.df$date<-chron(dates.=as.character(as.Date(tw.df$created)), format = "y-m-d")
 tw.df$weekday<-weekdays(tw.df$date)
 tw.df<-select(tw.df, -created)
 incidents_byDate<-tw.df %>% group_by(date) %>% summarise(n_tw = n())
+
+alldays<-seq(incidents_byDate$date[1], 
+             length=as.integer(max(incidents_byDate$date)-min(incidents_byDate$date)+1), 
+             by="+1 day")
 plot(incidents_byDate, type="l")
 incidents_byWeekday<-tw.df %>% group_by(weekday) %>% summarise(n_tw = n())
 plot(incidents_byWeekday)
 incidents_byHour<-tw.df %>% group_by(hour) %>% summarise(n_tw = n())
 plot(incidents_byHour)
+
+incidents_byDirection<-tw.df %>% group_by(direction) %>% summarise(n_tw = n())
+barplot(incidents_byDirection$n_tw, names.arg = incidents_byDirection$direction, xlab = "Direction", ylab = "Incidents")
+
+incidents_byTrainType<-tw.df %>% group_by(trainType) %>% summarise(n_tw = n())
+barplot(incidents_byTrainType$n_tw, names.arg = incidents_byTrainType$trainType, xlab = "Train type", ylab = "Incidents")
